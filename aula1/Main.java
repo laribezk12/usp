2aa3a154f1a15a1af0a20218ade60aa81cbad14f
laribezk12 <166923475+laribezk12@users.noreply.github.com>
2024-04-13 11:06:17 -0300

    Add files via upload

---
+++ aula1/Main.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	
+
+   char primeiraLetra = 'L';
+   
+   int idade = 16;
+   
+  
+    System.out.println("a primeira letra é:" +primeiraLetra);
+  
+    System.out.println("sua idade é:" +idade);
+    
+    int laranja = 16;
+    
+    int uva =  10;
+    
+   int resultado =  laranja + uva;
+   
+   System.out.println("A soma das frutas é: " +resultado);
+   
+   
+      }
+}
+
+
